c8e8b60e90a9477277ee218c936ea6e6e642a675
Vasudevan <vasudevan.r@krossark.com>
2025-07-11 21:39:07 +0530

    updated custom cors

--- app/Http/Middleware/GlobalCors.php
+++ app/Http/Middleware/GlobalCors.php
@@ -8,23 +8,23 @@ class GlobalCors
 {
     public function handle($request, Closure $next)
     {
+        $allowedOrigins = ['https://entekambolam.vercel.app', 'http://localhost:3000'];
+        $origin = $request->header('Origin');
         $headers = [
-            'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Accept, Authorization, X-Requested-With',
             'Access-Control-Allow-Credentials' => 'true',
         ];
-
+        if (in_array($origin, $allowedOrigins)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
         if ($request->getMethod() === 'OPTIONS') {
             return response()->json(['message' => 'CORS Preflight'], 200, $headers);
         }
-
         $response = $next($request);
-
         foreach ($headers as $key => $value) {
             $response->headers->set($key, $value);
         }
-
         return $response;
     }
 }
