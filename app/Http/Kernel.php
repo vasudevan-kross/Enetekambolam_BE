abb4602dd2a7caec38bbde86ae4248d6bd2b6032
Vasudevan <vasudevan.r@krossark.com>
2025-07-11 19:53:33 +0530

    updated with custom cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\GlobalCors::class,
-        \Fruitcake\Cors\HandleCors::class,
+        // \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\CORS::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
--- app/Http/Middleware/GlobalCors.php
+++ app/Http/Middleware/GlobalCors.php
@@ -8,12 +8,21 @@ class GlobalCors
 {
     public function handle($request, Closure $next)
     {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Accept, Authorization, X-Requested-With',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        if ($request->getMethod() === 'OPTIONS') {
+            return response()->json(['message' => 'CORS Preflight'], 200, $headers);
+        }
+
         $response = $next($request);
 
-        if (! $response->headers->has('Access-Control-Allow-Origin')) {
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
         }
 
         return $response;
