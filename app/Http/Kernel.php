d04736cb1f1a24f4c443ccb211a26d07a5822141
Vasudevan <vasudevan.r@krossark.com>
2025-07-12 19:56:13 +0530

    updated composer

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\GlobalCors::class,
+        \Fruitcake\Cors\HandleCors::class,
+        // \App\Http\Middleware\CORS::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
